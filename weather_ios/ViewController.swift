70acbc822279a998d506a8339050e959e4a3a282
Fernando Sousa <fnazarios@gmail.com>
2017-09-02 08:52:54 -0300

    Initial Commit

---
+++ weather_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather_ios
+//
+//  Created by Fernando Sousa on 02/09/17.
+//  Copyright © 2017 Fnazarios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
